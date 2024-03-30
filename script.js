7152dacbf1c833eb4789bc6aaedb6cfcbd53d097
AnnaMTerris <frontendwebdev32@gmail.com>
2024-03-30 21:05:28 +0000

    Movie subclass added

--- script.js
+++ script.js
@@ -44,3 +44,17 @@ class Media {
     }
   }
   
+  // create a Movie subclass
+class Movie extends Media {
+    constructor(title, director, runTime) {
+      super(title), 
+      this._director = director;
+      this._runTime = runTime;
+    }
+    get director() {
+      return this._director;
+    }
+    get runTime() {
+      return this._runTime;
+    }
+  }
