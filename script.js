78a9be7d1efc69fd7551f97c6f465b0f4c27fb29
AnnaMTerris <frontendwebdev32@gmail.com>
2024-03-30 21:03:47 +0000

    Script.js update with parent class: Media

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-
+// create parent class
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+    get title() {
+      return this._title;
+    }
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+    get ratings() {
+      return this._ratings;
+    }
+    set isCheckedOut(value) {
+      this._isCheckedOut = value;
+    }
+    toggleCheckOutStatus() {
+      this.isCheckedOut = !this.isCheckedOut;
+    }
+    getAverageRating() {
+      let averageSum = this._ratings.reduce((accumulator, rating) => accumulator + rating)
+      return averageSum / this._ratings.length;
+      }
+  
+    addRating(value) {
+      this._ratings.push(value);
+    }
+  }
