e012484b5b6d95616944c566f1dd8889684c6a7c
AnnaMTerris <frontendwebdev32@gmail.com>
2024-03-30 21:04:48 +0000

    Book subclass

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ class Media {
       this._ratings.push(value);
     }
   }
+
+  class Book extends Media {
+    constructor(title, author, pages) {
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+    get author() {
+      return this._author;
+    }
+    get pages() {
+      return this._pages;
+    }
+  }
+  
